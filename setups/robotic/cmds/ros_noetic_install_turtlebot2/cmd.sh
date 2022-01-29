1ebdfe6fccab6f07a19ee85cab5ce40fad73aef8
VeryHardBit <yanothaic8@gmaill.com>
2022-01-30 04:45:54 +0700

    Install turtlebot2 with progress

--- setups/robotic/cmds/ros_noetic_install_turtlebot2/cmd.sh
+++ setups/robotic/cmds/ros_noetic_install_turtlebot2/cmd.sh
@@ -3,8 +3,11 @@ echo "Please make sure that you're in ros workspace folder(not src)"
 echo "If you're already in workspace just hit [ENTER] or [CTRL-C] if you aren't yet"
 read
 
+echo "[Copying Makefile]"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cp "$SCRIPT_DIR/Makefile" .
+echo "[Install all of unmentioned dependencies]"
 make deps
 make
+echo "[Compile workspace]"
 catkin_make
