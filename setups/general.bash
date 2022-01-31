ed0f6fc96b99d40fb61b6615d2c877e0db6d598a
VeryHardBit <yanothaic8@gmaill.com>
2022-02-01 00:01:14 +0700

    God damn a lot of shit

--- setups/general.bash
+++ setups/general.bash
@@ -3,7 +3,6 @@ alias cls='clear'
 alias check_space='ncdu --exclude /mnt'
 
 #idc about sudo
-alias python='sudo python'
 alias rm_='sudo rm -r'
 
 #sudo apt-get install zip unzip
--- setups/robotic/cmds/use_ros_foxy.sh
+++ setups/robotic/cmds/use_ros_foxy.sh
@@ -1,5 +1,6 @@
 source /opt/ros/foxy/setup.bash
 alias ros2_create_pkg='ros2 pkg create --build-type ament_python $1'
+alias ros2_build_ws='colcon build'
 #source ~/turtlebot3_ws/install/setup.bash
 #source ~/construct_sim_ws/install/setup.bash
 source ~/ros_foxy_ws/devel/setup.bash
