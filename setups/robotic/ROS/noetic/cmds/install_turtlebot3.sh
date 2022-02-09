5748baa8138424e5460c79ff506a43c83f633a4a
VeryHardBit <yanothaic8@gmaill.com>
2022-02-10 01:19:28 +0700

    asd

--- setups/robotic/ROS/noetic/cmds/install_turtlebot3.sh
+++ setups/robotic/ROS/noetic/cmds/install_turtlebot3.sh
@@ -3,18 +3,17 @@
 set -o pipefail
 
 echo "Robot specification : Turtlebot3"
-echo "[WARNING] : This installation is quite long"
 echo "If you're ready just press [ENTER] or [CTRL-C] if you aren't yet"
 echo "If the following process doesn't install properly,please refer to https://emanual.robotis.com/docs/en/platform/turtlebot3/quick-start/ "
 read
 
 
-echo "[Updating]"
-sudo apt update
-sudo apt upgrade
+#echo "[Updating]"
+#sudo apt update -y
+#sudo apt upgrade -y
 
 echo "[Install dependencies packages]"
-sudo apt-get install ros-noetic-joy ros-noetic-teleop-twist-joy \
+sudo apt-get -y install ros-noetic-joy ros-noetic-teleop-twist-joy \
     ros-noetic-teleop-twist-keyboard ros-noetic-laser-proc \
     ros-noetic-rgbd-launch ros-noetic-rosserial-arduino \
     ros-noetic-rosserial-python ros-noetic-rosserial-client \
@@ -24,7 +23,7 @@ sudo apt-get install ros-noetic-joy ros-noetic-teleop-twist-joy \
     ros-noetic-gmapping ros-noetic-navigation ros-noetic-interactive-markers
 
 echo "[Install turtlebot3 packages]"
-sudo apt install ros-noetic-dynamixel-sdk  ros-noetic-turtlebot3-msgs ros-noetic-turtlebot3
+sudo apt install -y ros-noetic-dynamixel-sdk  ros-noetic-turtlebot3-msgs ros-noetic-turtlebot3
 
 echo "[Creating turtlebot2 workspace]"
 ws_name="turtlebot3_ws"
